427998a06266ff2a2395b6f35d8bc376bc27fb81
maoherve <maoherve8@gmail.com>
2020-04-22 15:21:56 +0200

    ajout who are us

---
+++ src/Model/WhoAreUsManager.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Model;
+
+class WhoAreUsManager extends AbstractManager
+{
+    const TABLE = 'whoAreUS';
+
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+
+    /**
+     * Get all row from database.
+     *
+     * @return array
+     */
+    public function selectAll(): array
+    {
+        return $this->pdo->query('SELECT * FROM ' . $this->table)->fetch();
+    }
+}
