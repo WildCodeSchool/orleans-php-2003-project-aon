39eb61be65a988e8a480d0f1eb2f16d4266c608c
mayard689 <doc-albert@laposte.net>
2020-04-27 18:32:50 +0200

    age and pool code replaced by clear text

--- src/Model/LessonManager.php
+++ src/Model/LessonManager.php
@@ -28,8 +28,11 @@ class LessonManager extends AbstractManager
     public function selectEverthingForOneById(int $activityId)
     {
         // prepared request
-        $statement = $this->pdo->prepare("SELECT * FROM $this->table RIGHT JOIN activity
-            ON lesson.activity_id=activity.id WHERE activity.id=:id ");
+        $statement = $this->pdo->prepare("SELECT *, pool.name AS pool_name FROM $this->table
+            RIGHT JOIN activity ON lesson.activity_id=activity.id 
+            JOIN pool ON lesson.pool_id=pool.id 
+            JOIN age ON lesson.age_id=age.id 
+            WHERE activity.id=:id ");
 
         $statement->bindValue('id', $activityId, \PDO::PARAM_INT);
         $statement->execute();
