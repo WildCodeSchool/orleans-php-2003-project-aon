14574baca7891e96d27ffaad9009bade677d3abd
maoherve <maoherve8@gmail.com>
2020-05-05 09:10:52 +0200

    supp

--- src/Controller/AdminLessonController.php
+++ src/Controller/AdminLessonController.php
@@ -11,4 +11,30 @@ class AdminLessonController extends AbstractController
         $message = urldecode($message);
         return $this->twig->render('Admin/addLesson.html.twig', ['message' => $message]);
     }
+
+        /**
+         * Display lesson creation page
+         *
+         * @return string
+         * @throws \Twig\Error\LoaderError
+         * @throws \Twig\Error\RuntimeError
+         * @throws \Twig\Error\SyntaxError
+         */
+    public function addLesson()
+    {
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $lessonManager = new LessonManager();
+            $lesson = [
+                'time' => $_POST['time'],
+                'day' => $_POST['day'],
+                'price' => $_POST['price'],
+            ];
+            $id = $lessonManager->insert($lesson);
+            header('Location:/Admin/index');
+        }
+
+        var_dump($lesson);
+        return $this->twig->render('Admin/addLesson.html.twig');
+    }
 }
