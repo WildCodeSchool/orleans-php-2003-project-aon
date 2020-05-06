dec856755dd3cff383d4e81de85e8c743760cbe5
maoherve <maoherve8@gmail.com>
2020-05-06 11:48:05 +0200

    création page stage

--- src/Model/StageManager.php
+++ src/Model/StageManager.php
@@ -14,14 +14,4 @@ class StageManager extends AbstractManager
     {
         parent::__construct(self::TABLE);
     }
-
-    /**
-     * Get all row from database.
-     *
-     * @return array
-     */
-    public function selectAll(): array
-    {
-        return $this->pdo->query('SELECT * FROM ' . $this->table)->fetchAll();
-    }
 }
